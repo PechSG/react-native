7039f46987550f3fb5e1748986fec643b1ef829a
Bunleap <bunleap.s@ligercambodia.org>
2019-12-12 09:59:42 +0700

    Fixing Profile Icon to right

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,16 +5,25 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 export default props =>
     <AppBar position="static">
         <Toolbar>
             <Typography variant="h6" color="inherit">Home</Typography>
+            <Grid
+                container
+                direction="row"
+                justify="flex-end"
+                alignItems="flex-end"
+            >
             <IconButton
                 aria-label="account of current user"
                 color="inherit"
               >
                 <AccountCircle />
               </IconButton>
+            </Grid>
         </Toolbar>
     </AppBar>
