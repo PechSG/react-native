4d0c037e3c6e7be8bd72e371b3388f7c43617a5c
Bunleap <bunleap.s@ligercambodia.org>
2019-12-12 08:53:32 +0700

    Edit Appbar

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -3,10 +3,20 @@ import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 export default props =>
     <AppBar position="static">
         <Toolbar>
-            <Typography variant="headline">PichSG</Typography>
+            <Typography variant="h6" color="inherit">Home</Typography>
+            <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
         </Toolbar>
     </AppBar>
