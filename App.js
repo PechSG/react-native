118f46b2ee6c98ccbc4f501049e9a40d13bc7b72
Bunleap <bunleap.s@ligercambodia.org>
2019-12-12 08:03:22 +0700

    Add Roboto Typeface

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import 'typeface-roboto';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
